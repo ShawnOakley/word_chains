4931c157c55ea7c0c82fce1d651c65de43dd7135
App Academy Student <student@appacademy.io>
2013-08-25 19:33:48 -0400

    Created recursive find_chain function.  Not yet functional.  Working on resolving an infinite loop issue.

--- word_chains.rb
+++ word_chains.rb
@@ -1,15 +1,17 @@
+require 'set'
+
 class WordChains
 
-  attr_accessor :dictionary
+  attr_accessor :dictionary, :already_visited
 
   def initialize
     self.dictionary = File.read('dictionary.txt').split(' ').join(' ')
-    p dictionary
+    self.already_visited = []
   end
 
   def adjacent_words(word, dictionary)
     # Uses regex, via helper function find_adjacency, to check and return an array of 'adjacent' words (i.e., words with only one letter difference). The final select expression filters out the original word.
-    dictionary.scan(/#{find_adjacency(word)}/).select{|word| word != word}
+    dictionary.scan(/#{find_adjacency(word)}/).select { |sim_word| sim_word != word }
   end
 
   def find_adjacency(word)
@@ -23,10 +25,34 @@ class WordChains
     full_regex = '\b(?:' + adjacency_array.join('|') + ')\b'
   end
 
+  def compare_with_previous(possible_targets)
+    p possible_targets
+    possible_targets = possible_targets.split(' ') - self.already_visited
+    self.already_visited += possible_targets
+    possible_targets
+  end
+
+
+  def find_chain(start_word, end_word, dictionary)
+    if start_word == end_word
+      return start_word
+    else
+      p adjacent_words(start_word, dictionary).compact
+      possible_targets = compare_with_previous(find_adjacency(start_word))
+      if possible_targets != nil
+        possible_targets.each do |word|
+          return start_word + "->" + find_chain(word, end_word, dictionary)
+        end
+      else
+        return nil
+      end
+    end
+  end
 
 end
 
 if __FILE__ == $0
   wchains = WordChains.new
-  p wchains.adjacent_words('berry', wchains.dictionary)
+#  p wchains.adjacent_words('cat', wchains.dictionary)
+  p wchains.find_chain('cat', 'rat', wchains.dictionary)
 end
