580f56498e8f95220e23aa5a8040ec010d7a8fe4
App Academy Student <student@appacademy.io>
2013-08-25 20:29:40 -0400

    Further edits

--- word_chains.rb
+++ word_chains.rb
@@ -1,95 +1,75 @@
-require 'set'
+dictionary = File.readlines('dictionary.txt').join(" ")
 
-class WordChains
+def find_adjacency (word)
+  adjacency_array = []
+  #word = word.split('')
+  (0...word.length).to_a.each do |char_index|
+    temp = word.dup
+    temp[char_index] = '\w'
+    adjacency_array << temp#.join('')
+  end
+  adjacency_array = '\b(?:' + adjacency_array.join('|') + ')\b'
+end
 
-  attr_accessor :dictionary, :visited_words, :chain_hash
 
-  def initialize
-    self.dictionary = File.read('dictionary.txt').split(' ').join(' ')
-    self.visited_words = []
-    self.chain_hash = {}
+def adjacent_words(word, dictionary)
+
+  adjacency_list = find_adjacency(word)
+  adjacent_word = dictionary.scan(/#{adjacency_list}/)
+
+end
+
+def find_chain(start_word, end_word, dictionary)
+  current_words = [start_word]
+  visited_words = [start_word]
+  chain_hash = { start_word => nil}
+
+  new_words = current_words.map do |word|
+    adjacent_words(word, dictionary)
   end
 
-  def adjacent_words(word, dictionary)
-    # Uses regex, via helper function find_adjacency, to check and return an array of 'adjacent' words (i.e., words with only one letter difference). The final select expression filters out the original word.
-    dictionary.scan(/#{find_adjacency(word)}/).select { |sim_word| sim_word != word }.uniq
-  end
+  new_words = new_words.flatten - visited_words
 
-  def find_adjacency(word)
-    # Creates a single regular expression of the form \\b(?:\wxx|x\wx|xx\w)\\b where \\b signifies the border of the word and the x represented a word's given character, and \w represents any character.  The | operator denotes 'or', meaning any one of the expressions will satisfy the regex.
-    adjacency_array = Array.new
-    word.size.times do |index|
-      regex = word.dup.split('')
-      regex[index] = '\w'
-      adjacency_array << regex.join('')
-    end
-    full_regex = '\b' + adjacency_array.join('|') + '\b'
-  end
+  #p new_words
 
-  def compare_with_previous(possible_targets)
-    # Compares with all previous targets to see if the word has already been introduced into the chain
-    possible_targets = possible_targets - self.visited_words
-    self.visited_words += possible_targets
-    possible_targets
-  end
+  until new_words.include?(end_word)
+  temp_words = new_words
 
-  def update_hash(target, origin)
-    if chain_hash[target] == nil
-      chain_hash[target] = origin
-    end
-  end
-
-
-  # Work on a recursive version of find_chain
-
-  # def find_chain(start_word, end_word, dictionary)
-  #   if start_word != end_word
-  #     possible_targets = compare_with_previous(adjacent_words(start_word, dictionary))
-  #       possible_targets.each do |word|
-  #       start_word + "->" + find_chain(word, end_word, dictionary).to_s
-  #     end
-  #   else
-  #     start_word
-  #   end
-  # end
-
-  # Non-recursive version of find_chain
-
-  def find_chain(start_word, end_word, dictionary)
-    current_words = [start_word]
-    visited_words = [start_word]
-    chain_hash = { start_word => nil}
-
-    possible_targets = current_words.map do |word|
-      adjacent_words(word, dictionary)
-    end
-
-    possible_targets = compare_with_previous(possible_targets)
-
-    until possible_targets.include?(end_word)
-      temp_words = possible_targets
-      current_words.each do |word|
-        individual_return = adjacent_words(word, dictionary).flatten
-        if word.is_a?(String)
-          compare_with_previous(individual_return).each do |found_word|
-            update_hash(found_word, word)
-          end
+    current_words.each do |word|
+      individual_return = adjacent_words(word, dictionary).flatten
+      if word.is_a?(String)
+        (individual_return.uniq - visited_words).each do |found_word|
+          chain_hash[found_word] = word if word != found_word
         end
-        possible_targets << compare_with_previous(individual_return)
       end
-      current_words = temp_words
-      (visited_words << current_words).flatten
-      possible_targets = compare_with_previous(possible_targets)
+      new_words << individual_return.uniq - visited_words
     end
-  chain_hash
+
+
+    current_words = temp_words
+    (visited_words << current_words).flatten
+    new_words = new_words.flatten.uniq - visited_words
+
   end
+  chain_hash
 end
 
-if __FILE__ == $0
-  wchains = WordChains.new
-  p wchains.find_chain('rat','cat', wchains.dictionary)
-#  p wchains.adjacent_words('cat', wchains.dictionary)
-#  p wchains.adjacent_words('rat', wchains.dictionary).count
-#  wchains.already_visited = ['lat','kat']
-#  p wchains.compare_with_previous(wchains.adjacent_words('rat', wchains.dictionary)).count
+def build_chain(visited_words, word)
+
+  if visited_words[word] == nil
+    return word
+  else
+    return word + " - " + build_chain(visited_words, visited_words[word])
+    #return visited_words[word].to_s + " - " + build_chain(visited_words, visited_words[word])
+    p "entered loop"
+  end
+
+  #visited_words
+
 end
+
+
+# puts adjacent_words('zoo', dictionary)
+p build_chain(find_chain('fume', 'lame', dictionary), "lame")
+
+
